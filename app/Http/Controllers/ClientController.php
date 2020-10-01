0a61bb126d07c171ef235a7541e826c5038ea825
Gonzalo Rodríguez <gonzalo.rodriguez@selectra.com>
2020-10-01 19:19:16 +0200

    refactor

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -41,29 +41,28 @@ class ClientController extends Controller
 
     public function store()
     {
-        echo $this->getRandomExpert();die;
         $fields = request()->validate([
-          'first_name' => 'required',
-          'last_name' => 'required',
-          'email' => 'required|email',
-          'phone' => [
+          'first_name'        => 'required',
+          'last_name'         => 'required',
+          'email'             => 'required|email',
+          'phone'             => [
             'required',
             'regex:/^(\+34|0034|34)?[\s|\-|\.]?[6-9][\s|\-|\.]?([0-9][\s|\-|\.]?){8}$/'
           ],
-          'net_income' => 'required',
-          'requested_amount' => 'required',
-          'time_slot' => 'required',
-          'expert' => 'required',
+          'net_income'        => 'required',
+          'requested_amount'  => 'required',
+          'time_slot'         => 'required',
+          'expert'            => 'required',
         ]);
 
         Client::create([
-          'first_name' => request('first_name'),
-          'email' => request('email'),
-          'phone' => request('phone'),
-          'net_income' => request('net_income'),
-          'requested_amount' => request('requested_amount'),
-          'time_slot' => request('time_slot'),
-          'expert' => $this->getRandomExpert(),
+          'first_name'        => request('first_name'),
+          'email'             => request('email'),
+          'phone'             => request('phone'),
+          'net_income'        => request('net_income'),
+          'requested_amount'  => request('requested_amount'),
+          'time_slot'         => request('time_slot'),
+          'expert'            => $this->getRandomExpert(),
         ]);
 
         return redirect()->route('landing_form');
