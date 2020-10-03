33053cd98b8de9e3fdbbbc3c5028360f51e65035
Gonzalo Rodríguez <gonzalo.rodriguez@selectra.com>
2020-10-03 17:19:58 +0200

    refactor

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -14,27 +14,27 @@ class ClientController extends Controller {
 
     public function store() {
         $fields = request()->validate([
-          'first_name'        => 'required',
-          'last_name'         => 'required',
-          'email'             => 'required|email',
-          'phone'             => [
-            'required',
-            'regex:/^(\+34|0034|34)?[\s|\-|\.]?[6-9][\s|\-|\.]?([0-9][\s|\-|\.]?){8}$/'
-          ],
-          'net_income'        => 'required|numeric',
-          'requested_amount'  => 'required|numeric',
-          'time_slot'         => 'required|numeric',
+            'first_name'        => 'required',
+            'last_name'         => 'required',
+            'email'             => 'required|email',
+            'phone'             => [
+              'required',
+              'regex:/^(\+34|0034|34)?[\s|\-|\.]?[6-9][\s|\-|\.]?([0-9][\s|\-|\.]?){8}$/'
+            ],
+            'net_income'        => 'required|numeric',
+            'requested_amount'  => 'required|numeric',
+            'time_slot'         => 'required|numeric',
         ]);
 
         Client::create([
-          'first_name'        => request('first_name'),
-          'last_name'         => request('last_name'),
-          'email'             => request('email'),
-          'phone'             => request('phone'),
-          'net_income'        => request('net_income'),
-          'requested_amount'  => request('requested_amount'),
-          'time_slot'         => request('time_slot'),
-          'expert'            => $this->getRandomExpert(),
+            'first_name'        => request('first_name'),
+            'last_name'         => request('last_name'),
+            'email'             => request('email'),
+            'phone'             => request('phone'),
+            'net_income'        => request('net_income'),
+            'requested_amount'  => request('requested_amount'),
+            'time_slot'         => request('time_slot'),
+            'expert'            => $this->getRandomExpert(),
         ]);
 
         return redirect()->route('home', ['status' => 1]);
